c8bd31e4fca437b6f0dc85634a2696b65d39ebdb
Анна Артемьева <artemiden@macbook-pro.myfiosgateway.com>
2022-07-13 21:28:25 -0400

    switch case

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
     setOperand2(input)
   }
 
+/*
   const plus = function(){
     setResult(parseInt(operand1) + parseInt(operand2))
   }
@@ -39,18 +40,66 @@ function App() {
   const fraction = function(){
     setResult(Math.round(parseInt(operand1) / parseInt(operand2)))
   }
+*/
 
+const clickHandler = function(item){
+  setResult(item)
+/*  if (item === "+"){
+    setResult(parseInt(operand1) + parseInt(operand2))
+  }
 
+  if (item === "-"){
+    setResult(parseInt(operand1) - parseInt(operand2))
+  }
+
+  if (item === "*"){
+    setResult(parseInt(operand1) * parseInt(operand2))
+  }
+
+  if (item === "/"){
+    setResult(parseInt(operand1) / parseInt(operand2))
+  }
+
+  if (item === "x^y"){
+    setResult(Math.pow(operand1,operand2))
+  }
+
+  if (item === "fraction division"){
+    setResult(Math.round(parseInt(operand1) / parseInt(operand2)))
+  }*/
+  switch (item) {
+    case "+":
+      setResult(parseInt(operand1) + parseInt(operand2))
+      break;
+    case "-":
+      setResult(parseInt(operand1) - parseInt(operand2))
+      break;
+    case "*":
+      setResult(parseInt(operand1) * parseInt(operand2))
+      break;
+    case "/":
+      setResult(parseInt(operand1) / parseInt(operand2))
+      break;
+    case "x^y":
+      setResult(Math.pow(operand1,operand2))
+      break;
+    case "fraction division":
+      setResult(Math.round(parseInt(operand1) / parseInt(operand2)))
+      break;
+    default:
+      alert( "Error" );
+  }
+}
 
   return (
   <div>
     <input placeholder={"operand1"} onInput={changeInput1}/>
-    <button onClick={plus}>+</button>
-    <button onClick={minus}>-</button>
-    <button onClick={multiplicate}>*</button>
-    <button onClick={divide}>/</button>
-    <button onClick={square}>x^y</button>
-    <button onClick={fraction}>fraction division</button>
+    <button onClick={() => clickHandler("+")}>+</button>
+    <button onClick={() => clickHandler("-")}>-</button>
+    <button onClick={() => clickHandler("*")}>*</button>
+    <button onClick={() => clickHandler("/")}>/</button>
+    <button onClick={() => clickHandler("x^y")}>x^y</button>
+    <button onClick={() => clickHandler("fraction division")}>fraction division</button>
     <input placeholder={"operand2"} onInput={changeInput2}/>
     <h1>{result}</h1>
   </div>
